5f811fb5fcb2b23dd1589e932deb8b9e8202c912
ye <ye@AJBZ-0148>
2016-08-16 08:54:28 +0800

    dsfa

--- src/test/java/demo/Test.java
+++ src/test/java/demo/Test.java
@@ -4,13 +4,15 @@ import static org.junit.Assert.*;
 
 public class Test {
 
-	@org.junit.Test
-	public void testName() throws Exception {
-		String[] array={
-				"CARDMESS","CARENTER","CHARGE","CARENTER","CHARGE",
-				"CARENTER","FAVTYPE","EVENTS","CARDRENEWAL","",
-				"CHARGE","FAVTYPE","CPRMORECARNO","FAVTYPE","AREARECORD","PREPAYCARDFEE","DEPOSITMONEYRECORD"};
-		System.out.println(array[0]);
-		System.out.println(array[17]);
+	static{
+		System.out.println("static first");
+	}
+	
+	static{
+		System.out.println("static second");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("main");
 	}
 }
